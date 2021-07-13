992cb165959bb302a8dc154b686add21040e9778
EvgeniyTyupa <boogee2820@gmail.com>
2021-07-13 11:27:00 +0300

    fcmt

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+
+const AblyChatComponent = dynamic(() => import('../components/AblyChatComponent'), { ssr: false });
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">Next.js Chat Demo</h1>
+        <AblyChatComponent />
+      </main>
+
+      <style jsx>{`
+        ...       
+      `}</style>
+
+      <style jsx global>{`
+        ...        
+      `}</style>
+    </div>
+  )
+}
